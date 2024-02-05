9a147156e29b2782dbc15e4da018a57d11833953
Albert Choi <albertc9981@gmail.com>
2024-02-05 00:41:46 -0500

    cs 506 exercise part c

--- .ipynb_checkpoints/polynomial-checkpoint.py
+++ .ipynb_checkpoints/polynomial-checkpoint.py
@@ -4,27 +4,36 @@ class X:
 
     def __repr__(self):
         return "X"
+    
+    def evaluate(self, x):
+        return x
 
 class Int:
     def __init__(self, i):
         self.i = i
-
+    
     def __repr__(self):
         return str(self.i)
+    
+     def evaluate(self, x):
+        return self.i
 
 class Add:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
-
+    
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
+    
+    def evaluate(self, x):
+        return self.p1.evaluate(x) + self.p2.evaluate(x)
 
 class Mul:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
-
+    
     def __repr__(self):
         if isinstance(self.p1, Add):
             if isinstance(self.p2, Add):
@@ -33,6 +42,10 @@ class Mul:
         if isinstance(self.p2, Add):
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
+    
+    def evaluate(self, x):
+        return self.p1.evaluate(x) * self.p2.evaluate(x)
+
 
 class Div:
     def __init__(self, p1, p2):
@@ -42,7 +55,9 @@ class Div:
     def __repr__(self):
         return "( " + repr(self.p1) + " ) / ( " + repr(self.p2) + " )"
 
-
+    def evaluate(self, x):
+        return self.p1.evaluate(x) / self.p2.evaluate(x)
+    
 class Sub:
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -50,9 +65,9 @@ class Sub:
     
     def __repr__(self):
         return "( " + repr(self.p1) + " ) - ( " + repr(self.p2) + " )"
+    
+    def evaluate(self, x):
+        return self.p1.evaluate(x) - self.p2.evaluate(x)
 
-
-poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
-print(poly)
-poly1 = Sub(Add(Int(4), Int(3)), Div(Int(8), X()))
-print(poly1)
+poly = Add(Add(Int(4), Int(3)), Add(X(), Mul(Int(1), Add(Mul(X(), X()), Int(1)))))
+print(poly.evaluate(-1))
